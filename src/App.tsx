3d7aa91ceba6fe19223e4ed3540503a4622855bb
hayatul-islam <hayatulemon340@gmail.com>
2022-06-15 01:37:51 +0600

    todo app done

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css';
 import Todos from './components/Todos';
 
 function App() {
+
   return (
-    <div className="App">
+    <>
       <Todos />
-    </div>
+    </>
   );
 }
 
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,7 @@
+import { Button, IconButton, Paper, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React, { useCallback, useEffect, useReducer, useRef } from 'react';
-
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 interface Todo {
     id: number,
     text: string
@@ -8,6 +10,7 @@ type ActionType = { type: 'ADD'; text: string } | { type: 'REMOVE'; id: number }
 const initialTodo: Todo[] = JSON.parse(localStorage.getItem('todos') || '');
 
 const Todos = () => {
+
     const reducer = (state: Todo[] = initialTodo, action: ActionType) => {
         switch (action.type) {
             case "ADD":
@@ -19,11 +22,13 @@ const Todos = () => {
                     }
                 ]
             case "REMOVE":
-                return state.filter(({ id }) => id !== action.id)
+                return state.filter(({ id }) => id !== action.id);
+            default:
+                return state
         }
     }
-    const [todos, dispatch] = useReducer(reducer, initialTodo);
 
+    const [todos, dispatch] = useReducer(reducer, initialTodo);
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos))
     }, [todos])
@@ -41,17 +46,61 @@ const Todos = () => {
 
 
     return (
-        <div className="App">
-            <input type="text" ref={newTodoRef} />
-            <button onClick={onAddTodo}>add</button>
-            {
-                todos.map(todo => <div key={todo.id}>
-                    {todo.text}
-                    <button onClick={() => dispatch({ type: 'REMOVE', id: todo.id })}>Remove</button>
-                </div>)
-            }
+        <>
+            <Box sx={{ display: 'flex', justifyContent: 'center', py: 5 }}>
+                <Box>
+                    <Box>
+                        <Typography sx={{ textAlign: 'center', pb: 3 }} variant='h5'>Todo App</Typography>
+                        <Paper
+                            component="form"
+                            sx={{ display: 'flex', alignItems: 'center', width: 460 }}
+                        >
+                            <input style={{ width: '400px', padding: '15px 20px', border: 'none', outline: 'none', fontSize: '20px' }} type="text" ref={newTodoRef} />
+                            <IconButton
+                                onClick={onAddTodo}
+                                sx={{
+                                    background: '#4383e6',
+                                    color: 'white',
+                                    fontSize: '16px',
+                                    padding: '18px 26px',
+                                    borderRadius: 0,
 
-        </div>
+                                    '&:hover': {
+                                        background: '#4383e6',
+                                        color: 'white',
+                                    }
+                                }}
+                                aria-label="directions">
+                                Add
+                            </IconButton>
+                        </Paper>
+                    </Box>
+                    <Box sx={{ pt: 5 }}>
+                        {
+                            todos.map(todo => <Box
+                                key={todo.id}
+                                sx={{
+                                    border: '1px solid #c2c4c9',
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    pl: '10px',
+                                    mb: 1
+                                }}
+                            >
+                                <Typography sx={{ fontSize: '20px' }}>{todo.text}</Typography>
+                                <button
+                                    onClick={() => dispatch({ type: 'REMOVE', id: todo.id })}
+                                    style={{ background: '#eaf1f3', border: 'none', padding: '10px 20px', }}
+                                >
+                                    <HighlightOffIcon style={{ fontSize: '30px', color: '#a75353' }} />
+                                </button>
+                            </Box>)
+                        }
+                    </Box>
+                </Box>
+            </Box>
+        </>
     );
 }
 
